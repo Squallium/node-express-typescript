e6ce7f9907b80a6404cc06642cbf8dcba7d955e8
Squallium <squallium3@gmail.com>
2016-03-25 18:02:47 +0100

    Addign heroku

---
+++ pre-prod-env.js
@@ -0,0 +1,10 @@
+/**
+ * Created by borja on 25/03/16.
+ */
+/// <reference path='./typings/main.d.ts' />
+var appModule = require('./app');
+var app = appModule.App;
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
+});
